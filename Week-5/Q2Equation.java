d6628c7bd89d70b2ff294badd92e821d340d2db9
Ryker Sun <rykersun2002@gmail.com>
2024-03-07 20:30:51 +0800

    Update Week-5

--- Week-5/Q2Equation.java
+++ Week-5/Q2Equation.java
@@ -16,17 +16,13 @@ public class Q2Equation {
 	}
 	
 	public double getRoot1() {
-		if (this.getDiscrimnant() > 0)
-			return ((-1 * b) + Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
-		if (this.getDiscrimnant() == 0)
+		if (this.getDiscrimnant() >= 0)
 			return ((-1 * b) + Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
 		return 0;
 	}
 
 	public double getRoot2() {
-		if (this.getDiscrimnant() > 0)
-			return ((-1 * b) - Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
-		if (this.getDiscrimnant() == 0)
+		if (this.getDiscrimnant() >= 0)
 			return ((-1 * b) - Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
 		return 0;
 	}
